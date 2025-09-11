7f6069ecac6d0a90025328836eb0436bed8df903
Kannan Priyadharshan <kpd2204@gmail.com>
2025-09-12 00:43:15 +0800

    Update config and downgraded to v3 Tailwind

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,10 +1,20 @@
-// tailwind.config.ts
 import type { Config } from "tailwindcss";
-export default {
+
+const config: Config = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
